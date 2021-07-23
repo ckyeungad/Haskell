fa2a0f751b23b971437f328da5b5d47fab2b4887
Ki <ckyeungad@gmail.com>
2021-07-23 21:58:54 +0800

    upload "do" function

---
+++ app/Main.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+module Main where
+
+import Lib
+m = "Candy wants a polly"
+c = "think"
+f = "4"
+main :: IO ()
+main =
+    do putStrLn "Hello, world!!"  --do the things at the same time
+       time
+       putStrLn "m"
+fast :: IO ()
+fast = 
+    do print (1+5)       
+
